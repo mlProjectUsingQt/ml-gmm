7f1c8f598d8762672a09cf880d62c7f1270eb040
tingianliu <tingianliu@gmail.com>
2011-12-11 23:18:06 +0000

    GMM is finished. Next step is to compute labels and weights for each cluster center given truth labels.

--- gmm.m
+++ gmm.m
@@ -9,10 +9,10 @@ loglikelihood_threshold = 1e-6;
 prev_loglikelihood = -1.0;
 [N D] = size(X);
 % Initialization
-% CV: K-by-D-by-D matrix, covariances for each cluster
+% CV: D-by-D-by-K matrix, covariances for each cluster
 % weights: K-by-1 vector, weight (pi_k) for each cluster
 [C CV weights] = gmm_init(X, K);
-weighted_gaussians = gaussian(X, C, CV) .* repmat(weights, 1, N);
+weighted_gaussians = gaussian(X, C, CV) .* repmat(weights', N, 1);
 sum_over_k_weighted_gaussians = sum(weighted_gaussians, 2);
 for itr = 1: max_iteration
   % E-step
@@ -26,47 +26,74 @@ for itr = 1: max_iteration
   % Estimate covariances (PRML: eq. 9.25)
   for k = 1: K
     diff = X - repmat(C(k, :), N, 1);
-    CV(k, :, :) = repmat(Z(:, k)', D, 1) .* diff' * diff;
+    CV(:, :, k) = (repmat(Z(:, k)', D, 1) .* diff') * diff / Nk(k);
   end
   % Estimate weights (PRML: eq. 9.26)
-  weights = Nk / N;
+  weights = Nk / N;  
+  
+%   [max_porb hard_assignment] = max(Z, [], 2);
+%   cluster1 = (hard_assignment == 1);
+%   cluster2 = (hard_assignment == 2);
+%   close;
+%   figure, grid on;
+%   hold on;
+%   scatter(X(cluster1, 1), X(cluster1, 2), 'r+');
+%   scatter(X(cluster2, 1), X(cluster2, 2), 'bo');
+%   scatter(C(1, 1), C(1, 2), 'ro', 'filled');
+%   scatter(C(2, 1), C(2, 2), 'bo', 'filled');
+%   hold off;
+%   pause();
+  
   % Update weighted gaussians and its sum-over-k
-  weighted_gaussians = gaussian(X, C, CV) .* repmat(weights, 1, N);
+  weighted_gaussians = gaussian(X, C, CV) .* repmat(weights', N, 1);
   sum_over_k_weighted_gaussians = sum(weighted_gaussians, 2);
   % Evaluate log likelihood
-  loglikelihood = sum(ln(sum_over_k_weighted_gaussians));
+  loglikelihood = sum(log(sum_over_k_weighted_gaussians));
   % Check convergence
   if abs(loglikelihood - prev_loglikelihood) < loglikelihood_threshold
     break;
   end
   prev_loglikelihood = loglikelihood;
 end
+disp([num2str(itr) ' iterations, log-likelihood = ' ...
+    num2str(loglikelihood)]);
 
 function prob = gaussian (X, means, covars)
 % Compute probabilities w.r.t. Gaussian distribution
 % prob: N-by-K matrix, prob(n, k) = Nor(X(N, :), means(k, :), covars(k, :, :))
 % X: N-by-D matrix, data points
 % means: K-by-D matrix, means
-% covars: K-by-D-by-D matrix, covariances
-[N D] = size(X);
+% covars: D-by-D-by-K matrix, covariances
+N = size(X, 1);
 K = size(means, 1);
 prob = zeros(N, K);
-coef = 1.0 / (2.0 * pi) ^ (D / 2.0);
 for k = 1: K
-  diff = X - repmat(means(k, :), N, 1);
-  cv = squeeze(covars(k, :, :));
-  prob(:, k) = exp(-0.5 * diff' / cv * diff) / sqrt(det(cv)) * coef;
+  prob(:, k) = mvnpdf(X, means(k, :), squeeze(covars(:, :, k)));
 end
 
 function [C CV weights] = gmm_init (X, K)
-% GMM initialization
+% Use k-means to initialize GMM
 % C: K-by-D matrix, cluster centers
-% CV: K-by-D-by-D matrix, covariances for each cluster
+% CV: D-by-D-by-K matrix, covariances for each cluster
 % weights: K-by-1 vector, weight (pi_k) for each cluster
 % X: N-by-D matrix, data points
 % K: scalar, # of clusters
+[idx C] = kmeans(X, K, 'replicates', 10, 'start', 'sample');
 [N D] = size(X);
-perm = randperm(N);
-perm = perm(1: K);
-C = X(perm, :);
-Z = zeros(N, 1);
+CV = zeros(D, D, K);
+weights = zeros(K, 1);
+for k = 1: K
+  Xk = X(idx == k, :);
+  Nk = size(Xk, 1);
+  diff = Xk - repmat(C(k, :), Nk, 1);
+  CV(:, :, k) = diff' * diff / Nk;
+  weights(k) = Nk / N;
+end
+% cluster1 = (idx == 1);
+% cluster2 = (idx == 2);
+% figure, hold on;
+% grid on;
+% scatter(X(cluster1, 1), X(cluster1, 2), 'r+');
+% scatter(X(cluster2, 1), X(cluster2, 2), 'bo');
+% scatter(C(1, 1), C(1, 2), 'ro', 'filled');
+% scatter(C(2, 1), C(2, 2), 'bo', 'filled');
--- run.m
+++ run.m
@@ -2,11 +2,39 @@ close all;
 clear all;
 clc;
 
-mu1 = [1.0 2.0];
-mu2 = [-1.0 -2.0];
-sigma1 = [3.0 0.2; 0.2 2.0];
-sigma2 = [2.0 0.0; 0.0 1.0];
-X = [mvnrnd(mu1, sigma1, 200); mvnrnd(mu2, sigma2, 100)];
-scatter(X(:, 1), X(:, 2), 10, 'ko');
+mean1 = [1.0 2.0];
+mean2 = [-1.0 -2.0];
+covar1 = [3.0 0.2; 0.2 2.0];
+covar2 = [2.0 0.0; 0.0 1.0];
+X = [mvnrnd(mean1, covar1, 200); mvnrnd(mean2, covar2, 100)];
+% scatter(X(:, 1), X(:, 2), 10, 'ko');
+% X = [-2 -2; -1 -2; 0 -2; -2 -1; 0 -1; -2 0; -1 0; ...
+%   1 0; 2 0; 0 1; 1 1; 2 1; 0 2; 1 2; 2 2];
+% X = [-2 -2; -1 -2; -2 -1; ...
+% X = [3 2; 2 0; 4 0; 3 -2; ...
+%   -4 2; -6 0; -3 0; -5 -2];
+%   1 0; 2 0; 0 1; 1 1; 2 1; 0 2; 1 2; 2 2];
+% figure, hold on;
+% grid on;
+% scatter(X(:, 1), X(:, 2), 10, 'ko');
+
+options = statset('Display', 'final');
+gm = gmdistribution.fit(X, 2, 'Options', options);
+figure, hold on;
+ezcontour(@(x, y)pdf(gm, [x y]));
+scatter(X(:, 1), X(:, 2), 'bo');
+grid on;
+hold off;
 
 [C Z] = gmm(X, 2);
+
+[max_porb hard_assignment] = max(Z, [], 2);
+cluster1 = (hard_assignment == 1);
+cluster2 = (hard_assignment == 2);
+figure, hold on;
+grid on;
+scatter(X(cluster1, 1), X(cluster1, 2), 'r+');
+scatter(X(cluster2, 1), X(cluster2, 2), 'bo');
+scatter(C(1, 1), C(1, 2), 'ro', 'filled');
+scatter(C(2, 1), C(2, 2), 'bo', 'filled');
+hold off;
