be799d76d95c4de9b5bb82d2b183fd801ebc3d00
tingianliu <tingianliu@gmail.com>
2011-12-13 08:07:08 +0000

    Use new dataset 'mnist8vs'. Still too slow.

--- gmm.m
+++ gmm.m
@@ -33,20 +33,6 @@ for itr = 1: max_iteration
   end
   % Estimate weights (PRML: eq. 9.26)
   weights = Nk / N;
-  
-%   [max_porb hard_assignment] = max(Z, [], 2);
-%   cluster1 = (hard_assignment == 1);
-%   cluster2 = (hard_assignment == 2);
-%   close;
-%   figure, grid on;
-%   hold on;
-%   scatter(X(cluster1, 1), X(cluster1, 2), 'r+');
-%   scatter(X(cluster2, 1), X(cluster2, 2), 'bo');
-%   scatter(C(1, 1), C(1, 2), 'ro', 'filled');
-%   scatter(C(2, 1), C(2, 2), 'bo', 'filled');
-%   hold off;
-%   pause();
-  
   % Update weighted gaussians and its sum-over-k
   weighted_gaussians = gaussian(X, C, CV) .* repmat(weights', N, 1);
   sum_over_k_weighted_gaussians = sum(weighted_gaussians, 2);
@@ -68,18 +54,18 @@ function prob = gaussian (X, means, covars)
 % means: K-by-D matrix, means
 % covars: D-by-D-by-K matrix, covariances
 % [N D] = size(X);
-N = size(X, 1);
+[N D] = size(X);
 K = size(means, 1);
 prob = zeros(N, K);
 for k = 1: K
-  prob(:, k) = mvnpdf(X, means(k, :), squeeze(covars(:, :, k)));
-%   sigma = squeeze(covars(:, :, k));
-%   mu = means(k, :);
-%   coef = (2 * pi) ^ (-D / 2) * det(sigma) ^ (-0.5);
-%   parfor n = 1: N
-%     diff = X(n, :) - mu;
-%     prob(n, k) = exp(-0.5 * diff * sigma * diff') * coef;
-%   end
+%   prob(:, k) = mvnpdf(X, means(k, :), squeeze(covars(:, :, k)));
+  sigma = squeeze(covars(:, :, k));
+  mu = means(k, :);
+  coef = (2 * pi) ^ (-D / 2) * det(sigma) ^ (-0.5);
+  parfor n = 1: N
+    diff = X(n, :) - mu;
+    prob(n, k) = exp(-0.5 * diff * sigma * diff') * coef;
+  end
 end
 
 function [C CV weights] = gmm_init (X, K)
@@ -92,52 +78,54 @@ function [C CV weights] = gmm_init (X, K)
 
 % opts = statset('MaxIter', 10);
 % [idx C] = kmeans(X, K, 'start', 'sample', 'options', opts);
+[idx C] = kmeans(X, K, 'start', 'sample');
+[N D] = size(X);
+CV = zeros(D, D, K);
+weights = zeros(K, 1);
+for k = 1: K
+  Xk = X(idx == k, :);
+  Nk = size(Xk, 1);
+  diff = Xk - repmat(C(k, :), Nk, 1);
+  CV(:, :, k) = diff' * diff / Nk;
+  weights(k) = Nk / N;
+end
+
 % [N D] = size(X);
+% C = zeros(K, D);
+% isPicked = zeros(N, 1);
+% idx = randi(N);
+% C(1, :) = X(idx, :);
+% isPicked(idx) = 1;
+% for k = 2: K
+%   C_bar = mean(C(1: k - 1, :), 1);
+%   dist2 = sum((X - repmat(C_bar, N, 1)) .^ 2, 2);
+%   dmax = -1.0;
+%   idx = 0;
+%   for n = 1: N
+%     if isPicked(n) == 0 && dist2(n) > dmax
+%       dmax = dist2(n);
+%       idx = n;
+%     end
+%   end
+%   C(k, :) = X(idx, :);
+%   isPicked(idx) = 1;
+% end
+% Z = zeros(N, 1);
+% for n = 1: N
+%   d2 = sum((repmat(X(n, :), K, 1) - C) .^ 2, 2);
+%   [dmax k] = min(d2);
+%   Z(n) = k;
+% end
 % CV = zeros(D, D, K);
 % weights = zeros(K, 1);
 % for k = 1: K
-%   Xk = X(idx == k, :);
+%   Xk = X(Z == k, :);
 %   Nk = size(Xk, 1);
 %   diff = Xk - repmat(C(k, :), Nk, 1);
 %   CV(:, :, k) = diff' * diff / Nk;
 %   weights(k) = Nk / N;
 % end
 
-[N D] = size(X);
-C = zeros(K, D);
-isPicked = zeros(N, 1);
-idx = randi(N);
-C(1, :) = X(idx, :);
-isPicked(idx) = 1;
-for k = 2: K
-  C_bar = mean(C(1: k - 1, :), 1);
-  dist2 = sum((X - repmat(C_bar, N, 1)) .^ 2, 2);
-  dmax = -1.0;
-  idx = 0;
-  for n = 1: N
-    if isPicked(n) == 0 && dist2(n) > dmax
-      dmax = dist2(n);
-      idx = n;
-    end
-  end
-  C(k, :) = X(idx, :);
-  isPicked(idx) = 1;
-end
-Z = zeros(N, 1);
-for n = 1: N
-  d2 = sum((repmat(X(n, :), K, 1) - C) .^ 2, 2);
-  [dmax k] = min(d2);
-  Z(n) = k;
-end
-CV = zeros(D, D, K);
-weights = zeros(K, 1);
-for k = 1: K
-  Xk = X(Z == k, :);
-  Nk = size(Xk, 1);
-  diff = Xk - repmat(C(k, :), Nk, 1);
-  CV(:, :, k) = diff' * diff / Nk;
-  weights(k) = Nk / N;
-end
 % cluster1 = (idx == 1);
 % cluster2 = (idx == 2);
 % figure, hold on;
