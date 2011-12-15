e780dc1497511c36ac30b66c2aee89adf39fbdfd
tingianliu <tingianliu@gmail.com>
2011-12-15 06:25:16 +0000

    Finished gmm.m; gmm_weight.m left to go.

--- gmm_test.m
+++ gmm_test.m
@@ -2,16 +2,21 @@ close all;
 clear all;
 clc;
 
-mean1 = [1 2];
-mean2 = [-1 -2];
-covar1 = [3 0.2; 0.2 2];
-covar2 = [2 0; 0 1];
-trX = [mvnrnd(mean1, covar1, 200); mvnrnd(mean2, covar2, 100)];
+% mean1 = [1 2];
+% mean2 = [-1 -2];
+% covar1 = [3 0.2; 0.2 2];
+% covar2 = [2 0; 0 1];
+% trX = [mvnrnd(mean1, covar1, 200); mvnrnd(mean2, covar2, 100)];
+% trX = [-6 0; -4 2; -5 -2; -3 0; 2 0; 3 2; 3 -2; 4 0];
+% figure;
+% scatter(trX(:, 1), trX(:, 2), 'ro', 'filled');
+% grid on;
 
-% dat = load('mnist8vs9train.mat');
-% trX = dat.X;
+dat = load('mnist8vs9train.mat');
+trX = dat.X;
 N = length(trX);
 K = floor(N / 100);
+% K = 2;
 [means Z] = gmm(trX, K);
 
 [max_prob, hard_assgn] = max(Z, [], 2);
